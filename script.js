ebfd0a9330a2e109d4d13d83ab67199811befef5
ryannarnoldd <ryannarnoldd@gmail.com>
2024-09-04 10:48:42 -0400

    Adds a way to increment seconds by clicking

--- script.js
+++ script.js
@@ -6,12 +6,21 @@ let hours = 0
 const hourHand = document.querySelector('[data-hour-hand]')
 const minuteHand = document.querySelector('[data-minute-hand]')
 const secondHand = document.querySelector('[data-second-hand]')
+// get seconds span to update the seconds.
+const secondsSpan = document.querySelector('.seconds')
 
 function rotateHand(hand, degree) {
     hand.style.setProperty('--rotation', degree)
 }
 
-setInterval( function renderClock () {
+function addSeconds() {
+    totalTime++
+    secondsSpan.innerHTML = totalTime
+    renderClock()
+
+}
+
+function renderClock () {
     seconds = totalTime % 60
     minutes = Math.floor(totalTime / 60) % 60
     hours = Math.floor(totalTime / 3600) % 12
@@ -19,12 +28,10 @@ setInterval( function renderClock () {
     rotateHand(secondHand, seconds * 6)
     rotateHand(minuteHand, minutes * 6)
     rotateHand(hourHand, hours * 30)
-
-    
-    totalTime++;
-}, 1000);
+};
 
 
 
 
 renderClock()
+
