e3887a3a70a2a4836d5f2f8ea803bd0da4467d47
ryannarnoldd <ryannarnoldd@gmail.com>
2024-09-04 11:52:54 -0400

    Added dynamic way to increase increment count

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let totalTime = 0 // In seconds for now.
+let totalTime = 34938
+let incrementInterval = 1
 let seconds = 0
 let minutes = 0
 let hours = 0
@@ -6,32 +7,24 @@ let hours = 0
 const hourHand = document.querySelector('[data-hour-hand]')
 const minuteHand = document.querySelector('[data-minute-hand]')
 const secondHand = document.querySelector('[data-second-hand]')
-// get seconds span to update the seconds.
 const secondsSpan = document.querySelector('.seconds')
 
-function rotateHand(hand, degree) {
-    hand.style.setProperty('--rotation', degree)
-}
-
-function addSeconds() {
-    totalTime++
+function increment() {
+    totalTime += incrementInterval;
     secondsSpan.innerHTML = totalTime
     renderClock()
-
-}
+};
 
 function renderClock () {
     seconds = totalTime % 60
     minutes = Math.floor(totalTime / 60) % 60
     hours = Math.floor(totalTime / 3600) % 12
 
-    rotateHand(secondHand, seconds * 6)
-    rotateHand(minuteHand, minutes * 6)
-    rotateHand(hourHand, hours * 30)
+    secondHand.style.setProperty('--rotation', seconds * 6)
+    minuteHand.style.setProperty('--rotation', minutes * 6)
+    hourHand.style.setProperty('--rotation', hours * 30)
 };
 
-
-
-
-renderClock()
-
+function doubleInterval() {
+    incrementInterval *= 2
+};
